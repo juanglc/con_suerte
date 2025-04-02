547d6361b49d1d77644841b46631b551012e6a45
juanglc <juanguiloco3@gmail.com>
2025-04-02 00:15:22 -0500

    commit

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
 from flask import Flask, render_template, redirect, url_for, session, jsonify, request
 from flask_session import Session
-from numero import generar_diccionario, escoger_numero, message_creation
 import os
+from random import sample, choices
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
+
+def generar_numero():
+    numero_gen = sample(range(10), 4)
+    return numero_gen
+
+def generar_diccionario():
+    diccionario = {}
+    while len(diccionario) < 5000:
+        numero = generar_numero()
+        numero_str = ''.join(map(str, numero))
+        if numero_str not in diccionario:
+            diccionario[numero_str] = {
+                'estado': choices([0, 1], weights=[0.7, 0.3], k=1)[0],
+                'fecha_hora': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                'valor_premio': '$150.000.000'
+            }
+    print(f"Diccionario generado con {len(diccionario)} elementos")
+    return diccionario
+
+def escoger_numero(dic):
+    winner_key = sample(list(dic.keys()), 1)[0]
+    winner = (winner_key, dic[winner_key])
+    print("Número Ganador: ", winner[0])
+    return winner
+
+def message_creation(winner):
+    if winner[1]['estado'] == 0:
+        message = f"El número {winner[0]} no fue comprado, por lo cual el premio no será repartido."
+        print(message)
+    else:
+        message = f"El número {winner[0]} fue comprado, por lo cual el premio será repartido al ganador."
+        print(message)
+    return message
 
 @app.route('/')
 def index():
@@ -56,7 +90,7 @@ def ganador(dic_num):
         session.modified = True  # Ensure the session is saved correctly
 
     message = message_creation(winner)
-    return render_template('ganador.html', dic_num=dic_num, winner=winner, message=message)
+    return jsonify({'dic_num': dic_num, 'winner': winner, 'message': message})
 
 @app.route('/regenerar')
 def regenerar():
