dc08a41700ad131d1a8bd657f968b50faa752136
juanglc <juanguiloco3@gmail.com>
2025-04-02 12:42:16 -0500

    commit

--- numero.py
+++ numero.py
@@ -7,7 +7,7 @@ def generar_numero():
 
 def generar_diccionario():
     diccionario = {}
-    while len(diccionario) < 5000:
+    while len(diccionario) < 10:
         numero = generar_numero()
         numero_str = ''.join(map(str, numero))
         if numero_str not in diccionario:
